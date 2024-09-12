1aaa80b0294b07b85b332be1918d209570e7f301
Waldemar Apostle <wapostlew@gmail.com>
2024-09-12 13:40:32 +0300

    refactor: grpc server runner

--- app/servers/grpc.py
+++ app/servers/grpc.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import asyncio
 import grpc
 from app import configs
@@ -6,7 +7,7 @@ from app.controllers.grpc import advertisement
 
 
 async def serve():
-    server = grpc.aio.server()
+    server = grpc.aio.server(concurrent.futures.ThreadPoolExecutor())
     advertisement_pb2_grpc.add_AdvertisementServiceServicer_to_server(
         advertisement.AdvertisementServicer(), server
     )
@@ -15,8 +16,16 @@ async def serve():
 
     await server.start()
     print(f"Server started, listening on {configs.grpc.host}:{configs.grpc.port}")
-    await server.wait_for_termination(15)
+
+    try:
+        await server.wait_for_termination()
+    except KeyboardInterrupt:
+        print("Server stopped!")
+    finally:
+        await server.stop(grace=5.0)
 
 
 if __name__ == "__main__":
+    import asyncio
+
     asyncio.run(serve())
